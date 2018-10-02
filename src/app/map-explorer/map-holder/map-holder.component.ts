941eabe895fbe8c9210e317e1a8423775cc3a79c
vpineda1996 <vpineda1996@gmail.com>
2018-10-02 13:04:45 -0700

    initial tests with mapbox

--- src/app/map-explorer/map-holder/map-holder.component.ts
+++ src/app/map-explorer/map-holder/map-holder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import mbx from 'mapbox-gl/dist/mapbox-gl.js';
 
 @Component({
   selector: 'app-map-holder',
@@ -10,6 +11,11 @@ export class MapHolderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    mbx.accessToken = 'pk.eyJ1IjoidnBpbmVkYTE5OTYiLCJhIjoiNWRmYjcxNTQyODFmNGM1MTJkMjg3OGQ3ODcyZDA5MTUifQ.l8nQjhBq_mmCrZ9mWDv9Yw';
+    var map = new mbx.Map({
+      container: 'dynamicMap',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
   }
 
 }
